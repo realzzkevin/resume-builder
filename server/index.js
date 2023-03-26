28d13e4211c71b4628657e89ab23dd7887f72ee7
zzkevin <realzzkevin@gmail.com>
2023-03-25 23:01:13 -0400

    update

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = 3100;
 const multer = require("multer");
 const path = require("path");
+const { configuration, OpenAIApi } = require("openai");
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -18,6 +20,25 @@ const storage = multer.diskStorage({
         cb(null, Date.now() + path.extname(file.originalname));
     },
 });
+const congiguration = new Configuration({
+    apikey: process.env.APIKEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const GPTFunction = async (text) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: text,
+        temperature: 0.6,
+        max_tokens: 250,
+        top_p: 1,
+        frequency_penalty: 1,
+        presence_penalty: 1,
+    });
+
+    return response.date.choices[0].text;
+}
 
 const upload = multer({
     storage: storage,
@@ -39,12 +60,38 @@ app.post("/resume/create", upload.single("headshotImage"), async(req, res) => {
         workHistory,
     } = req.body;
 
+    const workArray = JSON.parse(workHistory);
+
+    const newEntry = {
+        id: generateID(),
+        fullName,
+        image_rul : `http://localhost:3100/uploads/${req.file.filename}`,
+        currentPosition,
+        currentLength,
+        currentTechnologies,
+        workHistory: workArray,
+    };
+    
+    const remainderText = () => {
+        let stringText = "";
+        for (let i = 0; i < workArray.length; i++) {
+            stringText += ` ${workArray}`
+        }
+        return stringText;
+    }
+    
+    const prompt1 = `I am writing a resume, My details are \n 
+                    name: ${fullName} \n
+                    role: ${currentPosition} (${currentLength} years). \n
+                    I write in the technoloegies: ${currentTechnologies}. can you write a 100 words description for the top of the resume(first person Writing)?`;
+    const prompt2 = 
     console.log(req.body);
     res.json({
         message: "Request successful!",
         data:{},
     });
 });
+
 
 app.listen(PORT, () => {
     console.log(`server listening on ${PORT}`);
