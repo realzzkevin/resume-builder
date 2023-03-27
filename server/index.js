8734bd51242c49d1d3de9b040c385c7636cb170f
zzkevin <realzzkevin@gmail.com>
2023-03-26 22:20:32 -0400

    debug

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 3100;
 const multer = require("multer");
 const path = require("path");
-const { Configuration, OpenAIApi } = require("openai");
 const fs = require("fs");
+const { Configuration, OpenAIApi } = require("openai");
 
+// dotenv.config({path: path.resolve(__dirname, '../.env')});
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(cors());
 
-const key = process.env.APIKEY;
+const api_key = process.env.APIKEY;
 
 const generateID = () => Math.random().toString(36).substring(2, 10);
 
@@ -31,14 +34,14 @@ const upload = multer({
 });
 
 const configuration = new Configuration({
-   apikey: process.env.APIKEY,
+  apikey: process.env.APIKEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 const database = [];
 
-const GPTFunction = async (text) => {
+const ChatGPTFunction = async (text) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: text,
@@ -52,10 +55,37 @@ const GPTFunction = async (text) => {
   return response.data.choices[0].text;
 };
 
+const GPTFunction = async (text) => {
+  const header = new Headers();
+  header.append("content-Type", "application/json");
+  header.append("Authorization", "Bearer " + api_key);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+  const body = {
+    model: "text-davinci-003",
+    prompt: text,
+    temperature: 0.6,
+    max_tokens: 250,
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  };
+
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: header,
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data.choices[0].text;
+    })
+    .catch((error) => {
+      console.error("error", error);
+      return error;
+    });
+
+  // return response.data.choices[0].text;
+};
 
 app.post("/resume/create", upload.single("headshotImage"), async (req, res) => {
   const {
@@ -103,11 +133,12 @@ app.post("/resume/create", upload.single("headshotImage"), async (req, res) => {
                     } companies. ${remainderText()} \n
                     Can you write me 50 words for each company seprated in numbers of my succession in the company (in first person)?`;
 
-  const objective = await GPTFunction(prompt1);
-  const keypoints = await GPTFunction(prompt2);
-  const jobResponsibilities = await GPTFunction(prompt3);
+  const objective = await ChatGPTFunction(prompt1);
+  const keypoints = await ChatGPTFunction(prompt2);
+  const jobResponsibilities = await ChatGPTFunction(prompt3);
+
+  // const [objective, keypoints, jobResponsibilities] = await Promise.allSettled([GPTFunction(prompt1), GPTFunction(prompt2), GPTFunction(prompt3)]);
   const ChatGPtData = { objective, keypoints, jobResponsibilities };
-  console.log(ChatGPtData);
 
   const data = { ...newEntry, ...ChatGPtData };
   database.push(data);
@@ -117,6 +148,39 @@ app.post("/resume/create", upload.single("headshotImage"), async (req, res) => {
   });
 });
 
+app.post("/test", async (req, res) => {
+  const body = {
+    model: "text-davinci-003",
+    prompt:
+      "Hello, my name is John and I am a software developer. I am looking to build a new application to help with project management.",
+    temperature: 0.7,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  };
+
+  const header = new Headers();
+  header.append("content-Type", "application/json");
+  header.append("Authorization", "Bearer " + api_key);
+
+  const testreply = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: header,
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      res.json({
+        message: "request successful",
+        data,
+      });
+    })
+    .catch((error) => {
+      console.error("error", error);
+    });
+});
+
 app.listen(PORT, () => {
-  console.log(`server listening on ${PORT} ${key}`);
+  console.log(`server listening on ${PORT}`);
 });
